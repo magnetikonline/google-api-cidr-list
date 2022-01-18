27adafa18fb9aa17ae868c31e7a1526c4ca40351
Peter Mescalchin <peter@magnetikonline.com>
2022-01-18 15:57:08 +1100

    Reformatted wtih `black`

--- googleapicidrlist.py
+++ googleapicidrlist.py
@@ -4,99 +4,103 @@ import re
 import subprocess
 import sys
 
-GOOGLE_SPF_RECORD = '_spf.google.com'
+GOOGLE_SPF_RECORD = "_spf.google.com"
 
 
 def exit_error(message):
-	sys.stderr.write('Error: {0}\n'.format(message))
-	sys.exit(1)
+    sys.stderr.write("Error: {0}\n".format(message))
+    sys.exit(1)
 
-def exec_bin(command,*args):
-	try:
-		result = subprocess.check_output([command] + list(args))
-	except subprocess.CalledProcessError:
-		return False
 
-	return result.strip()
+def exec_bin(command, *args):
+    try:
+        result = subprocess.check_output([command] + list(args))
+    except subprocess.CalledProcessError:
+        return False
+
+    return result.strip()
+
 
 class QuerySPF(object):
-	SPF_RECORD_VALUE_REGEXP = re.compile(r'TXT[\t ]+"([^"]+)')
+    SPF_RECORD_VALUE_REGEXP = re.compile(r'TXT[\t ]+"([^"]+)')
 
-	def __init__(self,dig_path):
-		self._dig_path = dig_path
+    def __init__(self, dig_path):
+        self._dig_path = dig_path
 
-	def query(self,hostname):
-		# query TXT record via dig, extract record value
-		result = exec_bin(self._dig_path,'+noall','+answer','TXT',hostname)
-		if (not result):
-			return False
+    def query(self, hostname):
+        # query TXT record via dig, extract record value
+        result = exec_bin(self._dig_path, "+noall", "+answer", "TXT", hostname)
+        if not result:
+            return False
 
-		match = QuerySPF.SPF_RECORD_VALUE_REGEXP.search(result)
-		if (not match):
-			# no match
-			return False
+        match = QuerySPF.SPF_RECORD_VALUE_REGEXP.search(result)
+        if not match:
+            # no match
+            return False
 
-		# successfully extracted a record value
-		return match.group(1).lower()
+        # successfully extracted a record value
+        return match.group(1).lower()
+
 
 class GoogleApiCidrList(object):
-	def __init__(self,query):
-		self._query = query
+    def __init__(self, query):
+        self._query = query
 
-	def _parse_spf_rule(self,rule):
-		# build lists of SPF includes and CIDRs found
-		include_list = set()
-		cidr_list = set()
+    def _parse_spf_rule(self, rule):
+        # build lists of SPF includes and CIDRs found
+        include_list = set()
+        cidr_list = set()
 
-		for rule_part in rule.strip().split(' '):
-			if (rule_part.startswith('include:')):
-				# extract hostname from [include:] rule
-				include_list.add(rule_part[8:])
+        for rule_part in rule.strip().split(" "):
+            if rule_part.startswith("include:"):
+                # extract hostname from [include:] rule
+                include_list.add(rule_part[8:])
 
-			elif (rule_part.startswith('ip4:')):
-				# save CIDR
-				cidr_list.add(rule_part[4:])
+            elif rule_part.startswith("ip4:"):
+                # save CIDR
+                cidr_list.add(rule_part[4:])
 
-		return (include_list,cidr_list)
+        return (include_list, cidr_list)
 
-	def cidr_list(self,start_spf_hostname):
-		result_list = set()
-		include_seen = set()
+    def cidr_list(self, start_spf_hostname):
+        result_list = set()
+        include_seen = set()
 
-		# kick off querying, starting with [start_spf_hostname]
-		queue = [start_spf_hostname]
-		while (queue):
-			# shift SPF host item off queue
-			query_hostname = queue[0]
-			queue = queue[1:]
+        # kick off querying, starting with [start_spf_hostname]
+        queue = [start_spf_hostname]
+        while queue:
+            # shift SPF host item off queue
+            query_hostname = queue[0]
+            queue = queue[1:]
 
-			rule = self._query.query(query_hostname)
-			include_seen.add(query_hostname)
+            rule = self._query.query(query_hostname)
+            include_seen.add(query_hostname)
 
-			if (rule is not False):
-				# extract includes and CIDRs from rule - add unseen includes to queue
-				include_list,cidr_list = self._parse_spf_rule(rule)
-				queue.extend(list(include_list - include_seen))
+            if rule is not False:
+                # extract includes and CIDRs from rule - add unseen includes to queue
+                include_list, cidr_list = self._parse_spf_rule(rule)
+                queue.extend(list(include_list - include_seen))
 
-				# add CIDRs to collection
-				result_list.update(cidr_list)
+                # add CIDRs to collection
+                result_list.update(cidr_list)
 
-			else:
-				exit_error('unable to query [{0}]'.format(query_hostname))
+            else:
+                exit_error("unable to query [{0}]".format(query_hostname))
 
-		return sorted(result_list)
+        return sorted(result_list)
+
 
 def main():
-	# find path to dig
-	dig_path = exec_bin('which','dig')
-	if (dig_path is False):
-		exit_error('unable to locate dig')
+    # find path to dig
+    dig_path = exec_bin("which", "dig")
+    if dig_path is False:
+        exit_error("unable to locate dig")
 
-	# walk SPF record then list CIDRs extracted
-	api_range = GoogleApiCidrList(QuerySPF(dig_path))
-	for item in api_range.cidr_list(GOOGLE_SPF_RECORD):
-		print(item)
+    # walk SPF record then list CIDRs extracted
+    api_range = GoogleApiCidrList(QuerySPF(dig_path))
+    for item in api_range.cidr_list(GOOGLE_SPF_RECORD):
+        print(item)
 
 
-if (__name__ == '__main__'):
-	main()
+if __name__ == "__main__":
+    main()
--- test/googleapicidrlist.py
+++ test/googleapicidrlist.py
@@ -4,45 +4,45 @@ import os.path
 import sys
 import unittest
 
-sys.path.insert(0,os.path.realpath(os.path.dirname(__file__) + '/..'))
+sys.path.insert(0, os.path.realpath(os.path.dirname(__file__) + "/.."))
 import googleapicidrlist
 
 
 class ScriptTest(unittest.TestCase):
-	def test_api_range(self):
-		api_range = googleapicidrlist.GoogleApiCidrList(TestQuerySPF())
+    def test_api_range(self):
+        api_range = googleapicidrlist.GoogleApiCidrList(TestQuerySPF())
 
-		expected = [
-			'cidr-01',
-			'cidr-02',
-			'cidr-03',
-			'cidr-04',
-			'cidr-05',
-			'cidr-06',
-			'cidr-07',
-			'cidr-08',
-			'cidr-09',
-			'cidr-10'
-		]
+        expected = [
+            "cidr-01",
+            "cidr-02",
+            "cidr-03",
+            "cidr-04",
+            "cidr-05",
+            "cidr-06",
+            "cidr-07",
+            "cidr-08",
+            "cidr-09",
+            "cidr-10",
+        ]
 
-		self.assertEqual(
-			api_range.cidr_list(googleapicidrlist.GOOGLE_SPF_RECORD),
-			expected
-		)
+        self.assertEqual(
+            api_range.cidr_list(googleapicidrlist.GOOGLE_SPF_RECORD), expected
+        )
+
 
 class TestQuerySPF(object):
-	TEST_QUERY_HOSTNAME = {
-		googleapicidrlist.GOOGLE_SPF_RECORD: 'v=spf1 include:one.host',
-		'one.host': 'v=spf1 include:two.host include:three.host ~all',
-		'two.host': 'v=spf1 include:four.host ip4:cidr-01 ip4:cidr-02 ip4:cidr-03 ~all',
-		'three.host': 'v=spf1 include:one.host four.host ip4:cidr-04 ip4:cidr-05 ip4:cidr-06 ~all',
-		'four.host': 'v=spf1 include:two.host include:five.host ip4:cidr-02 ip4:cidr-07 ip4:cidr-08 ~all',
-		'five.host': 'v=spf1 ip4:cidr-09 ip4:cidr-10 ip4:cidr-01 ip4:cidr-04 ~all'
-	}
+    TEST_QUERY_HOSTNAME = {
+        googleapicidrlist.GOOGLE_SPF_RECORD: "v=spf1 include:one.host",
+        "one.host": "v=spf1 include:two.host include:three.host ~all",
+        "two.host": "v=spf1 include:four.host ip4:cidr-01 ip4:cidr-02 ip4:cidr-03 ~all",
+        "three.host": "v=spf1 include:one.host four.host ip4:cidr-04 ip4:cidr-05 ip4:cidr-06 ~all",
+        "four.host": "v=spf1 include:two.host include:five.host ip4:cidr-02 ip4:cidr-07 ip4:cidr-08 ~all",
+        "five.host": "v=spf1 ip4:cidr-09 ip4:cidr-10 ip4:cidr-01 ip4:cidr-04 ~all",
+    }
 
-	def query(self,hostname):
-		return TestQuerySPF.TEST_QUERY_HOSTNAME[hostname]
+    def query(self, hostname):
+        return TestQuerySPF.TEST_QUERY_HOSTNAME[hostname]
 
 
-if (__name__ == '__main__'):
-	unittest.main()
+if __name__ == "__main__":
+    unittest.main()
